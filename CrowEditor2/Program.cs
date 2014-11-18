1f238340c1d47dcf05cb903730edd8d68e58febc
Kutsarit Vanthavongsee <Kutsarit@web.de>
2014-11-18 20:30:28 +0100

    add hacked OpenTK version update CrowEditor2 test, now working add Gwen gui system

--- CrowEditor2/Program.cs
+++ CrowEditor2/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using Gwen.Sample.OpenTK;
 using OpenTK;
 using OpenTK.Graphics;
-using OpenTK.Graphics.OpenGL;
+using OpenTK.Graphics.OpenGL4;
 using OpenTK.Platform;
 
 namespace CrowEditor2
@@ -20,67 +17,208 @@ namespace CrowEditor2
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		[LoaderOptimization ( LoaderOptimization.MultiDomainHost )]
+		[LoaderOptimization ( LoaderOptimization.MultiDomain )]
 		static void Main ()
 		{
-			//Application.EnableVisualStyles ();
-			//Application.SetCompatibleTextRenderingDefault ( false );
-			//Application.Run ( new Form1 () );
-
-
-			OpenTK.Toolkit.Init ();
-
-			using ( var win = new MainWindow () )
-			using ( var sandbox = new Isolated<RemoteChildObj> () )
+			using ( var host = new EditorHost () )
 			{
-				win.Visible = true;
-				win.ProcessEvents ();
-
-				var hdc = wgl.GetCurrentDC ();
-				var hrc = wgl.GetCurrentContext ();
-				win.Context.MakeCurrent ( null );
-
-				Console.WriteLine ( "Start sandbox" );
-
-				var remote = sandbox.Value;
-				//var remote = new RemoteChildObj ();
-				remote.Width = win.Width;
-				remote.Height = win.Height;
-				remote.Init ( AppDomain.CurrentDomain, win.WindowInfo.Handle, hdc, hrc );
-
-				remote.Run ();
-
-				//win.MouseMove += ( a, b ) =>
-				//{
-				//	Console.WriteLine ( "Move" );
-				//};
-
-				//OpenTK.Platform.Utilities.
-
-				while ( !win.IsExiting )
-				{
-					win.ProcessEvents ();
-					Thread.Sleep ( 1 );
-				}
+				host.Run ();
 			}
+
+			//using ( var win = new MainWindow () )
+			//using ( var sandbox = new Isolated<EditorSandbox> () )
+			//{
+			//	win.Visible = true;
+
+			//	var hdc = wgl.GetCurrentDC ();
+			//	var hrc = wgl.GetCurrentContext ();
+			//	win.Context.MakeCurrent ( null );
+
+			//	Console.WriteLine ( "Start sandbox" );
+
+			//	var remote = sandbox.Value;
+			//	remote.Width = win.Width;
+			//	remote.Height = win.Height;
+			//	remote.Init ( AppDomain.CurrentDomain,
+			//		OpenTK.Graphics.OpenGL.GL.EntryPoints,
+			//		OpenTK.Graphics.OpenGL4.GL.EntryPoints,
+			//		hdc, hrc );
+
+			//	remote.Run ();
+
+			//	win.KeyUp += ( sender, e ) =>
+			//	{
+
+			//	};
+
+			//	while ( !win.IsExiting )
+			//	{
+			//		win.ProcessEvents ();
+			//		Thread.Sleep ( 1 );
+			//	}
+
+			//	remote.Exit ();
+			//}
 		}
 	}
 
-	class ChildSandbox
+	class EditorHost : IDisposable
+	{
+		GameWindow m_MainWindow;
+		IntPtr hDeviceContext;
+		IntPtr hRenderContext;
+
+		Isolated<EditorSandboxRemote> m_SandboxDomain;
+
+		public EditorHost ()
+		{
+			m_MainWindow = new GameWindow ();
+			m_MainWindow.Visible = true;
+
+			hDeviceContext = wgl.GetCurrentDC ();
+			hRenderContext = wgl.GetCurrentContext ();
+			m_MainWindow.Context.MakeCurrent ( null );
+
+			m_MainWindow.KeyUp += m_MainWindow_KeyUp;
+			m_MainWindow.Closing += m_MainWindow_Closing;
+
+			// init sandbox
+			StartEditorSandbox ();
+		}
+
+		public void Run ()
+		{
+			while ( !m_MainWindow.IsExiting )
+			{
+				m_MainWindow.ProcessEvents ();
+				Thread.Sleep ( 1 );
+			}
+			Dispose ();
+		}
+
+		public void Dispose ()
+		{
+			ReleaseEditorSandbox ();
+			m_MainWindow.Dispose ();
+		}
+
+		void StartEditorSandbox ()
+		{
+			ReleaseEditorSandbox ();
+			m_SandboxDomain = new Isolated<EditorSandboxRemote> ();
+			m_SandboxDomain.Value.Init ( AppDomain.CurrentDomain,
+				OpenTK.Graphics.OpenGL.GL.EntryPoints,
+				OpenTK.Graphics.OpenGL4.GL.EntryPoints,
+				hDeviceContext, hRenderContext );
+			m_SandboxDomain.Value.Run ();
+		}
+
+		void ReleaseEditorSandbox ()
+		{
+			if ( m_SandboxDomain != null )
+			{
+				m_SandboxDomain.Value.Exit ();
+				m_SandboxDomain.Dispose ();
+			}
+			m_SandboxDomain = null;
+		}
+
+		void m_MainWindow_KeyUp ( object sender, OpenTK.Input.KeyboardKeyEventArgs e )
+		{
+			if ( e.Key == OpenTK.Input.Key.P )
+			{
+				Debug.WriteLine ( "Playmode Restart sandbox" );
+				StartEditorSandbox ();
+			}
+			if ( e.Key == OpenTK.Input.Key.Escape )
+			{
+				m_MainWindow.Exit ();
+			}
+		}
+
+		void m_MainWindow_Closing ( object sender, System.ComponentModel.CancelEventArgs e )
+		{
+			ReleaseEditorSandbox ();
+		}
+
+	}
+
+	class EditorSandboxRemote : MarshalByRefObject
+	{
+		[NonSerialized]
+		private RemoteHostRemote Host;
+		[NonSerialized]
+		private EditorSandbox Logic;
+
+		public int Width;
+		public int Height;
+
+		public void Init ( AppDomain parent, IntPtr[] glFuncPtrs, IntPtr[] gl4FuncPtrs, IntPtr dc, IntPtr hdr )
+		{
+			var type = typeof ( RemoteHostRemote );
+			Host = (RemoteHostRemote)parent.CreateInstanceFromAndUnwrap (
+				type.Assembly.Location,
+				type.FullName
+			);
+			Host.TestHost ();
+
+			Logic = new EditorSandbox ( this, glFuncPtrs, gl4FuncPtrs, dc, hdr );
+
+			AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+			AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+		}
+
+		void CurrentDomain_UnhandledException ( object sender, UnhandledExceptionEventArgs e )
+		{
+			Debug.WriteLine ( "Appdomain Unhandled " + e.ExceptionObject );
+		}
+
+		void CurrentDomain_FirstChanceException ( object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e )
+		{
+			Debug.WriteLine ( "Appdomain FirstChanceException " + e.Exception );
+		}
+
+		public void Run ()
+		{
+			Logic.Run ();
+		}
+
+		public void Exit ()
+		{
+			Logic.Exit ();
+		}
+	}
+
+
+	class WindowHandler
+	{
+		public IntPtr hDeviceContext;
+		public IntPtr hRenderContext;
+	}
+
+
+	class EditorSandbox
 	{
 		private Thread Thread;
 		private bool Running = true;
-		private IntPtr window;
+		private IntPtr[] glFuncPtrs;
+		private IntPtr[] gl4FuncPtrs;
 		private IntPtr hdc;
 		private IntPtr hrc;
 		GraphicsContext dummyContext;
 		IWindowInfo dummyWindowInfo;
-		RemoteChildObj remote;
+		EditorSandboxRemote remote;
+		HelloGL3 hello;
+		SimpleWindow editorUI;
 
-		public ChildSandbox ( RemoteChildObj remote, IntPtr window, IntPtr hdc, IntPtr hrc )
+		public EditorSandbox ( EditorSandboxRemote remote,
+			IntPtr[] glFuncPtrs,
+			IntPtr[] gl4FuncPtrs,
+			IntPtr hdc, IntPtr hrc )
 		{
 			this.remote = remote;
-			this.window = window;
+			this.glFuncPtrs = glFuncPtrs;
+			this.gl4FuncPtrs = gl4FuncPtrs;
 			this.hdc = hdc;
 			this.hrc = hrc;
 		}
@@ -89,6 +227,7 @@ namespace CrowEditor2
 		{
 			Thread = new Thread ( Logic );
 			Thread.IsBackground = true;
+			Thread.SetApartmentState ( ApartmentState.STA );
 			Thread.Start ();
 		}
 
@@ -108,37 +247,51 @@ namespace CrowEditor2
 			MakeCurrent ();
 
 			dummyWindowInfo = OpenTK.Platform.Utilities.CreateDummyWindowInfo ();
-			dummyContext = new GraphicsContext ( new ContextHandle ( hrc ),
-				wgl.GetProcAddress,
-				() => new ContextHandle ( hrc ) );
+			dummyContext = new GraphicsContext ( new ContextHandle ( hrc ), null );
 			dummyContext.MakeCurrent ( dummyWindowInfo );
 
-			GL.Viewport ( 0, 0, remote.Width, remote.Height );
+			glFuncPtrs.CopyTo ( OpenTK.Graphics.OpenGL.GL.EntryPoints, 0 );
+			gl4FuncPtrs.CopyTo ( OpenTK.Graphics.OpenGL4.GL.EntryPoints, 0 );
 
+			//var ptr = wgl.GetProcAddress ( "glViewport" );
+			//Util.CheckWin32Error ();
+
+			hello = new HelloGL3 ();
+			hello.OnLoad ();
+
+			editorUI = new SimpleWindow ();
+			editorUI.OnLoad ( 640, 480 );
+
+			GL.ClearColor ( System.Drawing.Color.MidnightBlue );
+
+			GL.Viewport ( 0, 0, 640, 480 );
+
+			var timer = Stopwatch.StartNew ();
+			float deltaTime = 0;
 			while ( Running )
 			{
 				// render graphics
+
 				GL.Clear ( ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit );
 
-				GL.MatrixMode ( MatrixMode.Projection );
-				GL.LoadIdentity ();
-				GL.Ortho ( -1.0, 1.0, -1.0, 1.0, 0.0, 4.0 );
+				GL.Enable ( EnableCap.DepthTest );
 
-				GL.Begin ( PrimitiveType.Triangles );
+				hello.OnUpdateFrame ( deltaTime );
+				hello.OnRenderFrame ();
 
-				GL.Color3 ( Color.MidnightBlue );
-				GL.Vertex2 ( -1.0f, 1.0f );
-				GL.Color3 ( Color.SpringGreen );
-				GL.Vertex2 ( 0.0f, -1.0f );
-				GL.Color3 ( Color.Ivory );
-				GL.Vertex2 ( 1.0f, 1.0f );
 
-				GL.End ();
+				GL.Disable ( EnableCap.DepthTest );
+
+				editorUI.OnUpdateFrame ();
+				editorUI.OnRenderFrame ( 640, 480 );
 
 				wgl.SwapBuffers ( hdc );
 
-				Thread.Sleep ( 1 );
+				deltaTime = (float)timer.Elapsed.TotalSeconds;
+				timer.Restart ();
 			}
+
+			editorUI.Dispose ();
 		}
 
 		private void MakeCurrent ( bool bind = true )
@@ -153,30 +306,28 @@ namespace CrowEditor2
 		}
 	}
 
-	#region public static class wgl
 
-	public static class wgl
+	class RemoteHostRemote : MarshalByRefObject
 	{
-		[DllImport ( "opengl32.dll", EntryPoint = "wglGetCurrentDC" ), SuppressUnmanagedCodeSecurity]
-		public static extern IntPtr GetCurrentDC ();
-
-		[DllImport ( "opengl32.dll", EntryPoint = "wglGetCurrentContext" ), SuppressUnmanagedCodeSecurity]
-		public static extern IntPtr GetCurrentContext ();
-
-		[DllImport ( "opengl32.dll", EntryPoint = "wglCreateContext", SetLastError = true ), SuppressUnmanagedCodeSecurity]
-		public static extern IntPtr CreateContext ( IntPtr hdc );
-
-		[DllImport ( "opengl32.dll", EntryPoint = "wglMakeCurrent", SetLastError = true ), SuppressUnmanagedCodeSecurity]
-		public static extern int MakeCurrent ( IntPtr hdc, IntPtr hrc );
-
-		[DllImport ( "opengl32.dll", EntryPoint = "wglSwapBuffers", SetLastError = true ), SuppressUnmanagedCodeSecurity]
-		public static extern uint SwapBuffers ( IntPtr hdc );
-
-		[DllImport ( "opengl32.dll", EntryPoint = "wglGetProcAddress", SetLastError = true ), SuppressUnmanagedCodeSecurity]
-		public static extern IntPtr GetProcAddress ( string functionName );
+		public void TestHost ()
+		{
+			Console.WriteLine ( AppDomain.CurrentDomain.FriendlyName );
+			Console.WriteLine ( "Hallo host" );
+		}
 	}
 
-	#endregion
+	class MainWindow : GameWindow
+	{
+		public MainWindow ()
+			: base ( 640, 480,
+			GraphicsMode.Default,
+			"CrowEditor", GameWindowFlags.Default,
+			DisplayDevice.Default,
+			1, 0, GraphicsContextFlags.Default | GraphicsContextFlags.ForwardCompatible )
+		{
+
+		}
+	}
 
 	public sealed class Isolated<T> : IDisposable
 		where T : MarshalByRefObject
@@ -212,58 +363,169 @@ namespace CrowEditor2
 		}
 	}
 
-	class RemoteChildObj : MarshalByRefObject
+
+	class Util
 	{
-		[NonSerialized]
-		private RemoteHost Host;
-		[NonSerialized]
-		private ChildSandbox Logic;
-
-		public int Width;
-		public int Height;
-
-		public void Init ( AppDomain parent, IntPtr window, IntPtr dc, IntPtr hdr )
+		public static void CheckWin32Error ()
 		{
-			var type = typeof ( RemoteHost );
-			Host = (RemoteHost)parent.CreateInstanceFromAndUnwrap (
-				type.Assembly.Location,
-				type.FullName
-			);
-			Host.TestHost ();
-
-			Logic = new ChildSandbox ( this, window, dc, hdr );
+			int err = Marshal.GetLastWin32Error ();
+			if ( err != 0 )
+			{
+				throw new System.ComponentModel.Win32Exception ( err );
+			}
 		}
 
-		public void Run ()
-		{
-			Logic.Run ();
-		}
 
-		public void Exit ()
-		{
-			Logic.Exit ();
-		}
+		[DllImport ( "kernel32.dll" )]
+		internal static extern IntPtr GetProcAddress ( IntPtr handle, string funcname );
+
+		[DllImport ( "kernel32.dll" )]
+		internal static extern IntPtr GetProcAddress ( IntPtr handle, IntPtr funcname );
 	}
 
-	class RemoteHost : MarshalByRefObject
+	#region class PixelFormatDescriptor
+
+	[StructLayout ( LayoutKind.Sequential )]
+	public class PixelFormatDescriptor
 	{
-		public void TestHost ()
-		{
-			Console.WriteLine ( AppDomain.CurrentDomain.FriendlyName );
-			Console.WriteLine ( "Hallo host" );
-		}
+		public short size = PfdSize;
+		public short version = 1;
+		public PfdPixelFormat flags = PfdPixelFormat.DRAW_TO_WINDOW | PfdPixelFormat.SUPPORT_OPENGL | PfdPixelFormat.DOUBLEBUFFER;
+		public PfdPixelType pixelType = PfdPixelType.RGBA;
+		public byte colorBits = 32;
+		public byte redBits = 8;
+		public byte redShift;
+		public byte greenBits = 8;
+		public byte greenShift;
+		public byte blueBits = 8;
+		public byte blueShift;
+		public byte alphaBits = 8;
+		public byte alphaShift;
+		public byte accumBits = 0;
+		public byte accumRedBits;
+		public byte accumGreenBits;
+		public byte accumBlueBits;
+		public byte accumAlphaBits;
+		public byte depthBits = 16;
+		public byte stencilBits;
+		public byte auxBuffers;
+		public PfdLayerType layerType = PfdLayerType.MAIN_PLANE;
+		public byte reserved;
+		public int layerMask;
+		public int visibleMask;
+		public int damageMask;
+
+
+		private const short PfdSize = 40;
 	}
 
-	class MainWindow : GameWindow
-	{
-		public MainWindow ()
-			: base ( 640, 480,
-			GraphicsMode.Default,
-			"CrowEditor", GameWindowFlags.Default,
-			DisplayDevice.Default,
-			1, 0, GraphicsContextFlags.Default | GraphicsContextFlags.ForwardCompatible )
-		{
+	#endregion
 
-		}
+	#region enum PfdPixelFormat
+
+	[Flags]
+	public enum PfdPixelFormat : uint
+	{
+		DOUBLEBUFFER = 0x00000001,
+		STEREO = 0x00000002,
+		DRAW_TO_WINDOW = 0x00000004,
+		DRAW_TO_BITMAP = 0x00000008,
+		SUPPORT_GDI = 0x00000010,
+		SUPPORT_OPENGL = 0x00000020,
+		GENERIC_FORMAT = 0x00000040,
+		NEED_PALETTE = 0x00000080,
+		NEED_SYSTEM_PALETTE = 0x00000100,
+		SWAP_EXCHANGE = 0x00000200,
+		SWAP_COPY = 0x00000400,
+		SWAP_LAYER_BUFFERS = 0x00000800,
+		GENERIC_ACCELERATED = 0x00001000,
+		SUPPORT_DIRECTDRAW = 0x00002000,
+
+		/* PIXELFORMATDESCRIPTOR flags for use in ChoosePixelFormat only */
+		PFD_DEPTH_DONTCARE = 0x20000000,
+		PFD_DOUBLEBUFFER_DONTCARE = 0x40000000,
+		PFD_STEREO_DONTCARE = 0x80000000,
 	}
+
+	#endregion enum PfdPixelFormat
+
+	#region enum PfdPixelType
+
+	[Flags]
+	public enum PfdPixelType : byte
+	{
+		RGBA = 0,
+		COLORINDEX = 1
+	}
+
+	#endregion enum PfdPixelType
+
+	#region enum PfdLayerType
+
+	[Flags]
+	public enum PfdLayerType : byte
+	{
+		MAIN_PLANE = 0,
+		OVERLAY_PLANE = 1,
+		UNDERLAY_PLANE = 0xff
+	}
+
+	#endregion enum PfdLayerType
+
+	#region wgl
+
+	public static class wgl
+	{
+		[DllImport ( "opengl32.dll", EntryPoint = "wglGetCurrentDC" ), SuppressUnmanagedCodeSecurity]
+		public static extern IntPtr GetCurrentDC ();
+
+		[DllImport ( "opengl32.dll", EntryPoint = "wglGetCurrentContext" ), SuppressUnmanagedCodeSecurity]
+		public static extern IntPtr GetCurrentContext ();
+
+		[DllImport ( "opengl32.dll", EntryPoint = "wglCreateContext", SetLastError = true ), SuppressUnmanagedCodeSecurity]
+		public static extern IntPtr CreateContext ( IntPtr hdc );
+
+		[DllImport ( "opengl32.dll", EntryPoint = "wglMakeCurrent", SetLastError = true ), SuppressUnmanagedCodeSecurity]
+		public static extern int MakeCurrent ( IntPtr hdc, IntPtr hrc );
+
+		[DllImport ( "opengl32.dll", EntryPoint = "wglSwapBuffers", SetLastError = true ), SuppressUnmanagedCodeSecurity]
+		public static extern uint SwapBuffers ( IntPtr hdc );
+
+		[DllImport ( "opengl32.dll", EntryPoint = "wglGetProcAddress", SetLastError = true ), SuppressUnmanagedCodeSecurity]
+		public static extern IntPtr GetProcAddress ( string functionName );
+	}
+
+	#endregion
+
+	#region win32
+
+	public static class win32
+	{
+		[DllImport ( "gdi32", EntryPoint = "ChoosePixelFormat" ), System.Security.SuppressUnmanagedCodeSecurity]
+		public static extern int ChoosePixelFormat ( IntPtr hdc, PixelFormatDescriptor p_pfd );
+
+		[DllImport ( "gdi32", EntryPoint = "SetPixelFormat" ), System.Security.SuppressUnmanagedCodeSecurity]
+		public static extern bool SetPixelFormat ( IntPtr hdc, int iPixelFormat, PixelFormatDescriptor p_pfd );
+
+		[DllImport ( "gdi32", EntryPoint = "DescribePixelFormat" ), System.Security.SuppressUnmanagedCodeSecurity]
+		public static extern int DescribePixelFormat ( IntPtr deviceContext, int pixel, int pfdSize, PixelFormatDescriptor pixelFormat );
+
+		[DllImport ( "user32", EntryPoint = "GetDC", SetLastError = true ), System.Security.SuppressUnmanagedCodeSecurity]
+		public static extern IntPtr GetDC ( IntPtr hwnd );
+
+		[DllImport ( "user32", EntryPoint = "ReleaseDC" ), System.Security.SuppressUnmanagedCodeSecurity]
+		public static extern int ReleaseDC ( IntPtr hwnd, IntPtr dc );
+
+		[DllImport ( "user32.dll", SetLastError = true )]
+		public static extern int SetWindowLong ( IntPtr hWnd, int nIndex, int dwNewLong );
+
+		[DllImport ( "user32.dll", SetLastError = true )]
+		public static extern int GetWindowLong ( IntPtr hWnd, int nIndex );
+
+		[DllImport ( "user32.dll", SetLastError = true )]
+		public static extern IntPtr SetParent ( IntPtr hWndChild, IntPtr hWndNewParent );
+	}
+
+	#endregion
+
 }
---
+++ CrowEditor2/SimpleWindow.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using OpenTK;
+using OpenTK.Input;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using Gwen.Control;
+
+namespace Gwen.Sample.OpenTK
+{
+	/// <summary>
+	/// Demonstrates the GameWindow class.
+	/// </summary>
+	public class SimpleWindow
+	{
+		private Gwen.Input.OpenTK input;
+		private Gwen.Renderer.OpenTK renderer;
+		private Gwen.Skin.Base skin;
+		private Gwen.Control.Canvas canvas;
+		private UnitTest.UnitTest test;
+
+		const int fps_frames = 50;
+
+		private readonly List<long> ftime;
+		private readonly Stopwatch stopwatch;
+		private long lastTime;
+		private bool altDown = false;
+
+		public SimpleWindow ()
+		//: base ( 1024, 768 )
+		{
+			//Keyboard.KeyDown += Keyboard_KeyDown;
+			//Keyboard.KeyUp += Keyboard_KeyUp;
+
+			//Mouse.ButtonDown += Mouse_ButtonDown;
+			//Mouse.ButtonUp += Mouse_ButtonUp;
+			//Mouse.Move += Mouse_Move;
+			//Mouse.WheelChanged += Mouse_Wheel;
+
+			ftime = new List<long> ( fps_frames );
+			stopwatch = new Stopwatch ();
+		}
+
+		public void Dispose ()
+		{
+			canvas.Dispose ();
+			skin.Dispose ();
+			renderer.Dispose ();
+		}
+
+		/*
+		/// <summary>
+		/// Occurs when a key is pressed.
+		/// </summary>
+		/// <param name="sender">The KeyboardDevice which generated this event.</param>
+		/// <param name="e">The key that was pressed.</param>
+		void Keyboard_KeyDown ( object sender, KeyboardKeyEventArgs e )
+		{
+			if ( e.Key == global::OpenTK.Input.Key.Escape )
+				Exit ();
+			//else if ( e.Key == global::OpenTK.Input.Key.AltLeft )
+			//	altDown = true;
+			//else if ( altDown && e.Key == global::OpenTK.Input.Key.Enter )
+			//	if ( WindowState == WindowState.Fullscreen )
+			//		WindowState = WindowState.Normal;
+			//	else
+			//		WindowState = WindowState.Fullscreen;
+
+			input.ProcessKeyDown ( e );
+		}
+
+		void Keyboard_KeyUp ( object sender, KeyboardKeyEventArgs e )
+		{
+			altDown = false;
+			input.ProcessKeyUp ( e );
+		}
+
+		void Mouse_ButtonDown ( object sender, MouseButtonEventArgs args )
+		{
+			input.ProcessMouseMessage ( args );
+		}
+
+		void Mouse_ButtonUp ( object sender, MouseButtonEventArgs args )
+		{
+			input.ProcessMouseMessage ( args );
+		}
+
+		void Mouse_Move ( object sender, MouseMoveEventArgs args )
+		{
+			input.ProcessMouseMessage ( args );
+		}
+
+		void Mouse_Wheel ( object sender, MouseWheelEventArgs args )
+		{
+			input.ProcessMouseMessage ( args );
+		}
+		*/
+
+		public void OnLoad ( int Width, int Height )
+		{
+			renderer = new Gwen.Renderer.OpenTK ();
+			skin = new Gwen.Skin.TexturedBase ( renderer, "DefaultSkin.png" );
+			//skin = new Gwen.Skin.Simple(renderer);
+			//skin.DefaultFont = new Font(renderer, "Courier", 10);
+			canvas = new Canvas ( skin );
+
+			//input = new Input.OpenTK ( this );
+			//input.Initialize ( canvas );
+
+			canvas.SetSize ( Width, Height );
+			canvas.ShouldDrawBackground = false;
+			//canvas.BackgroundColor = Color.FromArgb ( 255, 150, 170, 170 );
+			//canvas.KeyboardInputEnabled = true;
+
+			test = new UnitTest.UnitTest ( canvas );
+
+			stopwatch.Restart ();
+			lastTime = 0;
+		}
+
+		public void OnResize ( int Width, int Height )
+		{
+			canvas.SetSize ( Width, Height );
+		}
+
+		public void OnUpdateFrame ()
+		{
+			if ( ftime.Count == fps_frames )
+				ftime.RemoveAt ( 0 );
+
+			ftime.Add ( stopwatch.ElapsedMilliseconds - lastTime );
+			lastTime = stopwatch.ElapsedMilliseconds;
+
+			if ( stopwatch.ElapsedMilliseconds > 1000 )
+			{
+				test.Note = String.Format ( "String Cache size: {0} Draw Calls: {1} Vertex Count: {2}", renderer.TextCacheSize, renderer.DrawCallCount, renderer.VertexCount );
+				test.Fps = 1000f * ftime.Count / ftime.Sum ();
+				stopwatch.Restart ();
+
+				if ( renderer.TextCacheSize > 1000 ) // each cached string is an allocated texture, flush the cache once in a while in your real project
+					renderer.FlushTextCache ();
+			}
+		}
+
+		public void OnRenderFrame ( int Width, int Height )
+		{
+			GL.MatrixMode ( MatrixMode.Projection );
+			GL.LoadIdentity ();
+			GL.Ortho ( 0, Width, Height, 0, -1, 1 );
+
+			canvas.RenderCanvas ();
+		}
+
+		/// <summary>
+		/// Entry point of this example.
+		/// </summary>
+		//[STAThread]
+		//public static void Main()
+		//{
+		//	using (SimpleWindow example = new SimpleWindow())
+		//	{
+		//		example.Title = "Gwen-DotNet OpenTK test";
+		//		example.VSync = VSyncMode.Off; // to measure performance
+		//		example.Run(0.0, 0.0);
+		//		//example.TargetRenderFrequency = 60;
+		//	}
+		//}
+	}
+}
